9e55cf0410719f269025e930996908c92e718270
Potta Sai Chaitanya <106024511+pottasaichaitanya@users.noreply.github.com>
2025-01-03 18:37:52 +0530

    updated small changes

--- PizzaSales.sql
+++ PizzaSales.sql
@@ -1,5 +1,179 @@
-SELECT COUNT(ORDER_ID) TOTAL_ORDERS FROM ORDERS;
-SELECT SUM(PIZZAS.PRICE*ORDER_DETAILS.QUANTITY) TOTAL_REVENUE FROM ORDER_DETAILS INNER JOIN PIZZAS ON ORDER_DETAILS.PIZZA_ID=PIZZAS.PIZZA_ID;
-SELECT PT.NAME,P.PRICE FROM PIZZA_TYPES PT INNER JOIN PIZZAS P ON PT.PIZZA_TYPE_ID=P.PIZZA_TYPE_ID WHERE P.PRICE=(SELECT MAX(PRICE) FROM PIZZAS);
-SELECT PIZZA_SIZE,COUNT(QUANTITY) PIZZAS_ORDERED FROM (SELECT * FROM PIZZAS INNER JOIN ORDER_DETAILS ON PIZZAS.PIZZA_ID=ORDER_DETAILS.PIZZA_ID) PIZZA_ORSER_DETAILS GROUP BY PIZZA_SIZE ORDER BY PIZZAS_ORDERED DESC FETCH FIRST 1 ROWS ONLY;
-SELECT PIZZA_TYPES.NAME,COUNT(ORDER_DETAILS.QUANTITY) TOTAL_ORDERS FROM PIZZA_TYPES  INNER JOIN PIZZAS  ON PIZZAS.PIZZA_TYPE_ID=PIZZA_TYPES.PIZZA_TYPE_ID JOIN ORDER_DETAILS ON PIZZAS.PIZZA_ID=ORDER_DETAILS.PIZZA_ID GROUP BY PIZZA_TYPES.NAME ORDER BY TOTAL_ORDERS DESC FETCH FIRST 1 ROWS ONLY;
+-- KPIs
+
+-- 1) Total Revenue
+
+SELECT *
+FROM pizzas
+WHERE pizza_id = 'big_meat_s'
+
+SELECT 
+ round(SUM(quantity * price), 2) AS [Total Revenue]
+FROM order_details AS o
+ JOIN pizzas AS p
+ ON o.pizza_id = p.pizza_id
+
+
+ -- 2) Average Order Value
+ -- total order value/order count
+
+ SELECT 
+  round(SUM(quantity * price)/COUNT(DISTINCT order_id), 2) AS [Average Order Value]
+ FROM order_details AS o
+ JOIN pizzas AS p
+ ON o.pizza_id = p.pizza_id
+
+
+
+ -- 3) Total Pizzas Sold
+
+ SELECT 
+  SUM(quantity) AS [Total Pizzas Sold]
+ FROM order_details;
+
+
+
+
+
+-- 4) Total Orders
+SELECT 
+COUNT(DISTINCT order_id) AS [Total Orders]
+ FROM order_details;
+
+
+
+
+
+
+
+-- 5) Average Pizza per Order
+-- pizzas sold/number of pizzas
+
+SELECT 
+ SUM(quantity)/COUNT(DISTINCT order_id) AS [Average Pizzas Per Order]
+FROM order_details;
+
+
+
+
+
+
+-- QUESTIONS TO ANSWER 
+
+-- 1) Daily Trends for Total Orders
+
+SELECT 
+ FORMAT( date, 'dddd') AS DayOfWeek
+ ,COUNT(DISTINCT order_id) AS total_orders
+FROM orders
+GROUP BY FORMAT( date, 'dddd')
+ORDER BY total_orders DESC;
+
+-- abbreviated day of week
+SELECT 
+    LEFT(FORMAT(date, 'dddd'), 3) AS AbbreviatedDayOfWeek,
+    COUNT(DISTINCT order_id) AS total_orders
+FROM 
+    orders
+GROUP BY 
+    FORMAT(date, 'dddd'),
+    LEFT(FORMAT(date, 'dddd'), 3)
+ORDER BY 
+    total_orders DESC;
+
+
+
+-- 2) Hourly Trend for Total Orders
+
+SELECT 
+ DATEPART(HOUR, time) AS [Hour]
+ ,COUNT(DISTINCT order_id) AS count
+FROM orders
+GROUP BY DATEPART(HOUR, time)
+ORDER BY [Hour];
+
+
+
+
+-- 3) Percentage of Sales by Pizza Category
+-- a: calculate total revenue per category
+-- % sales calculated as (a:/total revenue) * 100
+
+SELECT 
+category
+,SUM(quantity * price) AS revenue
+,round(SUM(quantity * price) * 100/(
+  SELECT SUM(quantity * price)
+  FROM pizzas AS p2
+  JOIN order_details AS od2 ON od2.pizza_id = p2.pizza_id
+  ), 2) AS percentage_sales
+FROM 
+pizzas AS p
+JOIN pizza_types AS pt ON p.pizza_type_id = pt.pizza_type_id
+JOIN order_details AS od ON od.pizza_id = p.pizza_id
+GROUP BY category
+ORDER BY percentage_sales DESC;
+
+
+
+-- 4) Percentage of Sales by Pizza Size
+SELECT 
+size
+,SUM(quantity * price) AS revenue
+,round(SUM(quantity * price) * 100/(
+  SELECT SUM(quantity * price)
+  FROM pizzas AS p2
+  JOIN order_details AS od2 ON od2.pizza_id = p2.pizza_id
+  ), 2) AS percentage_sales
+FROM 
+pizzas AS p
+JOIN pizza_types AS pt ON p.pizza_type_id = pt.pizza_type_id
+JOIN order_details AS od ON od.pizza_id = p.pizza_id
+GROUP BY size
+ORDER BY percentage_sales DESC;
+
+
+
+-- 5) Total Pizzas Sold by Pizza Category
+
+SELECT 
+category
+,SUM(quantity) AS quantity_sold
+FROM 
+pizzas AS p
+JOIN pizza_types AS pt ON p.pizza_type_id = pt.pizza_type_id
+JOIN order_details AS od ON od.pizza_id = p.pizza_id
+GROUP BY category
+ORDER BY SUM(quantity) DESC;
+
+
+
+-- 6) Top 5 Best Sellers by Total Pizzas Sold
+
+
+SELECT TOP 5
+name
+,SUM(quantity) AS total_pizzas_sold
+FROM 
+pizzas AS p
+JOIN pizza_types AS pt ON p.pizza_type_id = pt.pizza_type_id
+JOIN order_details AS od ON od.pizza_id = p.pizza_id
+GROUP BY name
+ORDER BY total_pizzas_sold DESC;
+
+
+
+
+
+
+-- 7) Bottom 5 Worst Sellers by Total Pizzas Sold
+SELECT TOP 5
+name
+,SUM(quantity) AS total_pizzas_sold
+FROM 
+pizzas AS p
+JOIN pizza_types AS pt ON p.pizza_type_id = pt.pizza_type_id
+JOIN order_details AS od ON od.pizza_id = p.pizza_id
+GROUP BY name
+ORDER BY total_pizzas_sold ASC;
+
+
